f687b4610e4aeb5e88d58ecd2f231efd7e9ac3fc
tposition <zetanalpha@gmail.com>
2022-04-16 23:37:49 +0800

    complete framework

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_firebase_login/app/app.dart';
-import 'package:flutter_firebase_login/auth_select/auth_select.dart';
-import 'package:flutter_firebase_login/common/widgets/bottom_nav_bar.dart';
-import 'package:flutter_firebase_login/info_update/view/view.dart';
+import '/app/app.dart';
+import '/auth_select/auth_select.dart';
+import '/common/widgets/bottom_nav_bar.dart';
+import '/info_update/view/view.dart';
 
 List<Page> onGenerateAppViewPages(
   final UserStatus state,
